54266f9a40c809e0b49025b03e19f15d5f7ea5a1
vitorvitor07 <vitorsilva_53@hotmail.com>
2023-08-22 17:19:32 -0300

    Discount class

--- src/ocp/classes/discount.ts
+++ src/ocp/classes/discount.ts
@@ -1,25 +1,21 @@
 export abstract class Discount {
-  abstract calculate(value: number): number;
-}
-
-export class FiftyPercentDiscount extends Discount {
-  private readonly discount = 0.5;
+  protected discount = 0;
 
   calculate(price: number): number {
     return price - price * 0.5;
   }
 }
 
+export class FiftyPercentDiscount extends Discount {
+  protected readonly discount = 0.5;
+}
+
 export class TenPercentDiscount extends Discount {
-  private readonly discount = 0.5;
+  protected readonly discount = 0.1;
 
   calculate(price: number): number {
     return price - price * 0.1;
   }
 }
 
-export class NoDiscount extends Discount {
-  calculate(price: number): number {
-    return price;
-  }
-}
+export class NoDiscount extends Discount {}
